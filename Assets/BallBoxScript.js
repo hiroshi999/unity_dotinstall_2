804f3476e46ff6234d55b865bff1e294d7f3ad37
takada <qwer4404@gmail.com>
2014-11-05 00:42:43 +0900

    first commit

---
+++ Assets/BallBoxScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var ball : Transform;
+var n : int = 0;
+
+function Update () {
+	// スペース入力を受け付ける
+	if (Input.GetButtonUp("Jump")) {
+		Instantiate(ball, transform.position, transform.rotation);
+		n++;
+	}
+
+	if (n > 5) {
+		Application.LoadLevel("GameOver");
+	}
+}
---
+++ Assets/PlayerScript.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+//var x : int = 10;
+
+function Start () {
+	//Debug.Log("Hello World => " + x);
+	Debug.Log("Script Start!!");
+}
+
+function Update () {
+
+	// 位置情報の変更
+	//transform.position.z += 0.1;
+	//transform.position += Vector3(0,0,0.1);
+	//transform.Translate(0,0,0.1);
+
+	// スペース入力を受け付ける
+	if (Input.GetButtonUp("Jump")) {
+		Debug.Log("jumped");
+	}
+
+	// 左右キーで移動
+	var x : float = Input.GetAxis("Horizontal");
+	transform.Translate(x * 0.2,0,0);
+}
+
+function OnCollisionEnter (obj : Collision) {
+	if (obj.gameObject.name == 'RightWall') {
+		Debug.Log("Hit R!!");
+	}
+}
